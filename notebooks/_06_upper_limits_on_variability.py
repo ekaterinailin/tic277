0147b567d15d8963bcfde179f2c814cdf68a73bb
Ekaterina Ilin <eilin@aip.de>
2023-06-09 10:42:27 +0200

    flare only solution, sinusoidals removed

--- notebooks/_06_upper_limits_on_variability.py
+++ notebooks/_06_upper_limits_on_variability.py
@@ -22,37 +22,30 @@ import corner
 
 import matplotlib.pyplot as plt
 
-
-def sin2_and_flare(x, a, b, c, d, e, f):
-    """Sinusoidal function with first harmonic period."""
-    return a * np.sin(4 * np.pi * x + b) + c + aflare(x, d, e, f)
-
-def sin_and_flare(x, a, b, c, d, e, f):
-    """Sinusoidal function with fixed period."""
-    return a * np.sin(2. * np.pi * x + b) + c + aflare(x, d, e, f)
+def flare_only(x, d, e, f, a):
+    """Flare only."""
+    return aflare(x, d, e, f) + a
 
 
-def log_prior(theta):
+def log_prior_aflare(theta):
     """Log prior function."""
-    a, b, c, d, e, f = theta
-    if ((0 < a < 1e-4) &
-        (0 < b < 2*np.pi) &
-        (0 < c < 1) &
-        (0 < d < 2) & 
-        (0 < e < 10) & 
-        (0 < f < 1)):
+    d, e, f, a = theta
+    # print(theta)
+    if ((0.2 < d < .4) & 
+        (0 < e < 2) & 
+        (0 < f < 1e-4) &
+        (0 < a < 1e-4) ):
         return 0.0
     return -np.inf
 
-def log_probability(theta, x, y, yerr):
-    """Log probability function."""
-    lp = log_prior(theta)
 
+def log_probability_aflare(theta, x, y, yerr):
+    """Log probability function."""
+    lp = log_prior_aflare(theta)
+    # print(lp)
     if not np.isfinite(lp):
         return -np.inf
-    return lp + log_likelihood(theta, x, y, yerr)
-
-
+    return lp + log_likelihood_aflare(theta, x, y, yerr)
 
 
 if __name__ == "__main__":
@@ -60,26 +53,28 @@ if __name__ == "__main__":
     # read in the lightcurve
     dd = pd.read_csv("../results/stacked_xray_lightcurve.csv")
 
-    model_func = sin_and_flare
-    n = 1
-
-    def log_likelihood(theta, x, y, yerr):
+    model_func = flare_only
+    n = 3
+    
+    def log_likelihood_aflare(theta, x, y, yerr):
         """Log likelihood function."""
-        a, b, c, d, e, f = theta
-        model = model_func(x, a, b, c, d, e, f)
+        d, e, f, a = theta
+        model = model_func(x, d, e, f, a)
         sigma2 = yerr**2
         return -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2))
 
     # define shortcuts to the relevant columns    
-    x = dd.rot_phase_unfold.values
+    x = dd.time.values / 60 / 60 / 24 - 8982.
     y = dd.events_minus_bkg.values
-    yerr = np.nan
+    yerr = dd["std"].values
 
     # find starting point for MCMC with scipy.optimize
-    nll = lambda *args: -log_likelihood(*args)
-    initial = np.array([1e-5, 0.5, 1e-5, 0.4, .3, 3e-5]) + 0.1 * np.random.randn(6)
+    nll = lambda *args: -log_likelihood_aflare(*args)
+    initial = np.array([0.38, 0.2, 4e-5, 5e-6]) + 1e-6 * np.random.randn(4) #1e-5, 0.5, 1e-5,
     soln = minimize(nll, initial, args=(x, y, yerr))
-    a, b, c, d, e, f = soln.x
+    d, e, f, a = soln.x #a, b, c, 
+
+    print(d,e,f,a)
 
     # set all solx to at least 1e-7 if they are negative to avoid failing MCMC
     soln.x[soln.x < 1e-7] = 1e-6
@@ -90,23 +85,24 @@ if __name__ == "__main__":
     # -------------------------------------------------------------------------
     # MCMC
 
-    pos = soln.x + 1e-7 * np.random.randn(32, 6)
+    pos = soln.x + 1e-6 * np.random.randn(32, 4)
     nwalkers, ndim = pos.shape
 
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability,
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability_aflare,
                                     args=(x, y, yerr))
     
     # run the MCMC
-    N = 100000
+    N = 50000
     sampler.run_mcmc(pos, N, progress=True)
 
     # -------------------------------------------------------------------------
     # plot the chains
 
-    fig, axes = plt.subplots(6, figsize=(10, 7), sharex=True)
+    fig, axes = plt.subplots(4, figsize=(10, 7), sharex=True)
     samples = sampler.get_chain()
     labels = ["amplitude", "phase offset", "base level", 
               "flare peak", "flare FWHM", "flare amplitude"]
+    labels = labels[-3:] + ["baseline"]
     for i in range(ndim):
         ax = axes[i]
         ax.plot(samples[:, :, i], "k", alpha=0.3)
@@ -117,7 +113,7 @@ if __name__ == "__main__":
     axes[-1].set_xlabel("step number")
 
     plt.tight_layout()
-    plt.savefig(f"../results/plots/mcmc_sinusoidal{n}_chains.png", dpi=300)
+    plt.savefig(f"../results/plots/mcmc_flareonly_chains.png", dpi=300)
 
     # -------------------------------------------------------------------------
     # make a corner plot
@@ -133,7 +129,7 @@ if __name__ == "__main__":
                            title_kwargs={"fontsize": 12},)
 
     plt.tight_layout()
-    plt.savefig(f"../results/plots/mcmc_sinusoidal{n}_corner.png", dpi=300)
+    plt.savefig(f"../results/plots/mcmc_flareonly_corner.png", dpi=300)
 
     # -------------------------------------------------------------------------
     # error bars
@@ -145,7 +141,7 @@ if __name__ == "__main__":
         results[label] = mcmc
 
     # save the results
-    results.to_csv(f"../results/mcmc_sinusoidal{n}_results.csv", index=False)
+    results.to_csv(f"../results/mcmc_flareonly_results.csv", index=False)
 
     # -------------------------------------------------------------------------
     # plot the best fit model
@@ -166,12 +162,12 @@ if __name__ == "__main__":
     # layout
     plt.xlim(x[0], x[-1])
     plt.ylim(-1e-5, 7e-5)
-    plt.xlabel("time [rotation periods]")
+    plt.xlabel("time [d]")
     plt.ylabel("X-ray flux - background [counts/s]")
     plt.tight_layout()
 
     # save to file
-    plt.savefig(f"../results/plots/mcmc_sinusoidal{n}_best_fit.png", dpi=300)
+    plt.savefig(f"../results/plots/mcmc_flareonly_best_fit.png", dpi=300)
 
     # -------------------------------------------------------------------------
     # print the relevant results
@@ -182,6 +178,6 @@ if __name__ == "__main__":
     # -------------------------------------------------------------------------
     # save flat_samples to file
 
-    np.savetxt(f"../results/mcmc_sinusoidal{n}_flat_samples.csv", flat_samples, delimiter=",")
+    np.savetxt(f"../results/mcmc_flareonly_flat_samples.csv", flat_samples, delimiter=",")
 
 
